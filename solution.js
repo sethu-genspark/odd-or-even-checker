c1e8095ba24e9259bc9c15a0cec81cc82b7fc5f7
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2025-03-11 12:13:00 +0530

    ADD - Code to check whether 5 is odd or even.

--- solution.js
+++ solution.js
@@ -1,1 +1,10 @@
-console.log(`Hello World`)
+let numberToBeChecked = 5
+
+if (numberToBeChecked % 2 == 0)
+{
+    console.log(`It's an even number`)
+}
+else
+{
+    console.log(`It's an odd number`)
+}
