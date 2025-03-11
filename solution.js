b0a5dcfc9ba85a2fd46452f01ec2b9d4fbeebe98
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2025-03-11 12:20:48 +0530

    UPDATE. Implemented a function to check odd or even.

--- solution.js
+++ solution.js
@@ -1,10 +1,13 @@
-let numberToBeChecked = 5
+function checkOddOrEven(numberToBeChecked)
+{
+    if (numberToBeChecked % 2 == 0)
+    {
+        console.log(`It's an even number`)
+    }
+    else
+    {
+        console.log(`It's an odd number`)
+    }
+}
 
-if (numberToBeChecked % 2 == 0)
-{
-    console.log(`It's an even number`)
-}
-else
-{
-    console.log(`It's an odd number`)
-}
+checkOddOrEven(3)
